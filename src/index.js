f1dc40de917d027ec7c00a32cfc0ec191997e549
jeet kanodia <jeetkanodia@gmail.com>
2023-12-06 19:42:37 +0530

    add: slash command register route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Routes } from "discord.js";
 import { config } from "dotenv";
+import { REST } from "@discordjs/rest";
 
 config();
 
@@ -7,12 +8,56 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-client.on("ready", (c) => {
-  console.log(`Logged in as ${c.user.tag}!`);
+const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
+
+client.on("ready", (c) => console.log(`Logged in as ${c.user.tag}!`));
+
+client.on("messageCreate", (message) =>
+  console.log(message.author.username, message.guild.name)
+);
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
 });
 
-client.on("messageCreate", (message) => {
-  message.reply("Pong!");
-});
+async function main() {
+  const commands = [
+    {
+      name: "ping",
+      description: "Replies with Pong!",
+    },
+  ];
+  try {
+    console.log("Started refreshing application (/) commands.");
 
-client.login(process.env.BOT_TOKEN);
+    // use this if you want to register commands globally
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+      body: commands,
+    });
+
+    // use this if you want to register commands in a guild
+
+    // await rest.put(
+    //   Routes.applicationGuildCommands(
+    //     process.env.CLIENT_ID,
+    //     process.env.GUILD_ID
+    //   ),
+    //   {
+    //     body: commands,
+    //   }
+    // );
+
+    client.login(process.env.BOT_TOKEN);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
