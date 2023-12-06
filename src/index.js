56249671ad4ccf633a48ee8b84ae267e0db9f35b
jeet kanodia <jeetkanodia@gmail.com>
2023-12-06 16:53:13 +0530

    Add message reply functionality and example.env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import { config } from "dotenv";
+
 config();
+
 const client = new Client({
-  intents: ["Guilds", "GuildMessages"],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
 client.on("ready", (c) => {
   console.log(`Logged in as ${c.user.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+  message.reply("Pong!");
 });
 
 client.login(process.env.BOT_TOKEN);
