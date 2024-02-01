dcbae0a5505bb88680e8b048261fa2a76bd4636a
Varun Gotmare <varunpgotmare@gmail.com>
2024-02-01 16:13:08 +0530

    add: embed command

--- src/commands/utility/embed.js
+++ src/commands/utility/embed.js
@@ -1,13 +1,12 @@
 const {EmbedBuilder} = require("discord.js");
 const { SlashCommandBuilder } = require("discord.js");
-
+const channelID = "1202558613164400721";
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("embed")
     .setDescription("A template for setup embed?"),
   async execute(interaction) {
     const exEmbed = new EmbedBuilder()
-        .seColor("#00ff00")
         .setTitle("Title")
         .setDescription("Description")
         .addFields(
@@ -15,9 +14,12 @@ module.exports = {
               { name: "Field 2", value: "Value 2", inline: true },
               { name: "Field 3", value: "Value 3", inline: true }, 
         )
-        .setFooter("Footer")
+        .setFooter({text: "Footer"})
         .setTimestamp()
 
-    const message = await interaction.reply({ embeds: exEmbed });
+    // const message = await interaction.reply({ embeds: exEmbed });
+    //send embed to channel
+    const channel = await interaction.client.channels.fetch(channelID);
+    channel.send({ embeds: [exEmbed] });
   },
 };
